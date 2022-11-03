a55106b5b0c741b49ee8e40efeb8ab14b0c10b84
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-03 08:52:43 +0800

    starter project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Learn Node: File Upload & Download
+
+##### Course:
+
+- [NodeJS - The Complete Guide](https://www.udemy.com/course/nodejs-the-complete-guide)
+
+##### Application Scope:
+
+- S20: Node File Upload & Download
+- Topics discussed: Handling Multipart Form Data, Handling file with Multer, Filtering Files with Mimetype, Storing File Data to Database, Serving Images Statically, Downloading Files with Auth, Setting File Type Headers, Restricting FIle Access, Using PDFKit for pdf Generation
---
+++ app.js
@@ -0,0 +1,106 @@
+// Libraries
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+// Routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+// Controllers
+const errorController = require('./controllers/error');
+
+// Models
+const User = require('./models/user');
+
+dotenv.config();
+
+const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_CONNECTION_STRING,
+  collection: 'sessions',
+});
+
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // Throwing new error on sync task
+  // will be handled on our error handling middleware
+  // throw new Error('Sync Error');
+
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+      // throw new Error will not work for async task
+      // throw new Error(err);
+
+      // the Error should be passed inside next()
+      // for our error handling middleware to handle it
+      next(new Error(err));
+    });
+});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use('/500', errorController.get500);
+app.use(errorController.get404);
+
+// express error handling middleware
+app.use((error, req, res, next) => {
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
