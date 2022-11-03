f8d00f077c12d6009ca1e278e8b15855df625e82
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-04 07:42:16 +0800

    329. restricting file access

--- controllers/shop.js
+++ controllers/shop.js
@@ -145,20 +145,37 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const { orderId } = req.params;
-  const invoiceName = `invoice-${orderId}.pdf`;
-  const invoicePath = path.join('data', 'invoices', invoiceName);
 
-  fs.readFile(invoicePath, (err, data) => {
-    if (err) {
-      return next(err);
-    }
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error('No invoie found'));
+      }
 
-    // user will be able to download the invoice pdf file with its invoiceName
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader(
-      'Content-Disposition',
-      `attachment; filename="${invoiceName}"`
-    );
-    res.send(data);
-  });
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error('Unauthorized access of invoice file'));
+      }
+
+      const invoiceName = `invoice-${orderId}.pdf`;
+      const invoicePath = path.join('data', 'invoices', invoiceName);
+
+      fs.readFile(invoicePath, (err, data) => {
+        if (err) {
+          return next(err);
+        }
+
+        // user will be able to download the invoice pdf file with its invoiceName
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader(
+          'Content-Disposition',
+          `attachment; filename="${invoiceName}"`
+        );
+        res.send(data);
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
